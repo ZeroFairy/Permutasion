4772d62d510da2cad95da16fc94796e09f9f825b
ZeroFairy <jordanjv453@gmail.com>
2025-03-14 02:29:22 +0700

    Added a few method for solving problem D (Unfinised)
    Sleepy...

--- src/main/java/permutation/ShowAllNode.java
+++ src/main/java/permutation/ShowAllNode.java
@@ -36,8 +36,11 @@ public class ShowAllNode {
         this.hosts.add(host);
     }
 
-    //Recursive
-    public void recAllNode(Node host) {
+    /**
+     * Recursive
+     * @param hosts --> for expected group of host
+     */
+    private void recAllNode(Node host, LinkedList<Node> hosts) {
         if (!this.hold.get(host)) {
             this.hold.put(host, true);
             this.current.add(host);
@@ -47,8 +50,8 @@ public class ShowAllNode {
                 this.history.add(new LinkedList<>(this.current));
 
                 if (this.current.size() != this.size) {
-                    for (Node nextHost : this.hosts) {
-                        this.recAllNode(nextHost);
+                    for (Node nextHost : hosts) {
+                        this.recAllNode(nextHost, hosts);
                     }
                 }
             }
@@ -58,24 +61,110 @@ public class ShowAllNode {
         }
     }
 
-    public void startRec() {
+    /**
+     * Recursive untuk merge list
+     */
+    private void recList() {
+
+    }
+
+    //Result ambil dari history saja
+    private void mergeResults (Map<String, LinkedList<Node>> hostGroups) {
+        for (Object key : hostGroups.keySet()) {
+
+        }
+    }
+
+    private LinkedList<Node> getFromHistory(int size) {
+        LinkedList<Node> result = new LinkedList();
+        for (LinkedList<Node> history : this.history) {
+            if (history.size() == size) {
+                result.addAll(history);
+            }
+        }
+        return result;
+    }
+
+    private Map<String, LinkedList<Node>> splitHostGroup() {
+        Map<String, LinkedList<Node>> result = new HashMap<>();
+
+        for(Node host : this.hosts) {
+            String first = Character.toString(this.detectFirstChar(host.getLabel()));
+
+            if (result.get(first) == null) {
+                LinkedList<Node> group = new LinkedList<>();
+                group.add(host);
+                result.put(first, group);
+            } else {
+                LinkedList<Node> group = result.get(first);
+                group.add(host);
+                result.put(first, group);
+            }
+        }
+
+        return result;
+    }
+
+    private char detectFirstChar(String input) {
+        return input.charAt(0);
+    }
+
+    //Untuk soal A
+    private void startARec() {
         this.hold = new HashMap<>();
-        this.resetHold();
+        this.resetHold(this.hosts);
 
         for (Node host : this.hosts) {
-            this.recAllNode(host);
+            this.recAllNode(host, this.hosts);
         }
 
         this.output();
     }
 
-    public void resetHold() {
-        for (Node host : this.hosts) {
+    private void startDRec() {
+        this.hold = new HashMap<>();
+        Map<String, LinkedList<Node>> fromHist = new HashMap<>();
+        Map<String, LinkedList<Node>> split = this.splitHostGroup();
+
+        for (Object key : split.keySet()) {
+            LinkedList<Node> group = split.get(key);
+
+            for (Node host : group) {
+                this.recAllNode(host, group);
+            }
+
+            //Bias atur untuk size dari history
+            fromHist.put(key.toString(), this.getFromHistory(group.size()));
+            this.resetHold(group);
+            this.history.clear();
+        }
+
+        this.mergeResults(fromHist);
+        this.output();
+    }
+
+    public void start(String start) {
+        switch (start.toUpperCase()) {
+            case "SOAL_A":
+                startARec();
+//            case "SOAL_B":
+//                startBRec();
+//            case "SOAL_C":
+//                startCRec();
+            case "SOAL_D":
+                startDRec();
+            default:
+                System.out.println("Sorry, tidak ada pilihan ini");
+        }
+    }
+
+    private void resetHold(LinkedList<Node> groupHost) {
+        for (Node host : groupHost) {
             this.hold.put(host, false);
         }
     }
 
-    public void output() {
+    private void output() {
         int counter = 0;
         for (LinkedList<Node> history : this.history) {
             if (history.size() == this.size) {
